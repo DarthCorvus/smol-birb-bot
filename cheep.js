325e08e87a2c7cf49a49a9e1bf49e35b635f55e0
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-05 18:35:55 -0500

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -12,6 +12,9 @@ client.on('message', message => {
   else if (message.content === 'Smol Birb, what are you?') {
   message.reply("oh haiiiii!!! I'm just a smol birb, silly :hatching_chick: Don't mind me ^~^ Cheep!");
     }
+  else if (message.content === 'Smol Birb, how flexible are you?') {
+  message.reply("Well I can't touch my toes, if that's what you mean >.< Oh, and I can't respond to anything that isn't worded juuuust right, so please be specific! Sorry about that!");
+    }
   });
   
 client.login(process.env.BOT_TOKEN);
