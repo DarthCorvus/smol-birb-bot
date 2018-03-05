6b0fb6c6a84aff81bd36204138b0d2e951621c03
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-05 03:19:02 -0500

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   
 client.on('message', message => {
   if (message.content === 'Cheep!') {
-    message.reply('Chirp! ^~^');
+    message('Chirp! ^~^');
     }
   });
   
