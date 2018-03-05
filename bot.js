9f9de395a85c07e89ec56b1f23f23510e34ba27d
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-04 21:53:47 -0500

    Create bot.js
    Smol Birb's basic code v.1

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+  });
+  
+  client.on('message', message => {
+    if (message.content === 'Cheep!' || 'cheep!' || 'cheep' || 'Cheep') {
+      message.reply('Chirp! ^~^');
+    }
+  });
+  
+client.login(process.env.BOT_TOKEN);
