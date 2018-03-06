82a7812be8d3c65dfbc9a6ed01af1a1359ceff49
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-06 01:23:43 -0500

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -26,12 +26,12 @@ client.on('message', message => {
     message.channel.send('You silly goose!'),
     message.channel.send('You forgot your own name?'),
     message.channel.send('Your name is ${member}, silly xD');
+  }
   else if (message.content === 'Smol Birb, join my channel') {
     voiceChannel.join()
       .then(connection => {
         const dispatcher = connection.playFile('339326__inspectorj__bird-whistling-a.wav');
       })
-  }
   });
   
 client.login(process.env.BOT_TOKEN);
