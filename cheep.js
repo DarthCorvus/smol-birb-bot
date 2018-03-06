17d9f39e320a8899433f25623e75c7203b68aa9e
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-05 21:49:13 -0500

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   
 client.on('message', message => {
   if (message.content === 'Cheep!') {
-  message.channel.sendMessage('Chirp! ^~^');
+  message.channel.send('Chirp! ^~^');
     }
   else if (message.content === 'Smol Birb, what are you?') {
   message.reply("oh haiiiii!!! I'm just a smol birb, silly :hatching_chick: Don't mind me ^~^ Cheep!");
