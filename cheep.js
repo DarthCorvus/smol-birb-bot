74fea4d9103cf4597f11d5993b4cc2d2fc76d969
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-07 20:01:52 -0500

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -9,30 +9,6 @@ client.on('message', message => {
   if (message.content === 'Cheep!') {
   message.channel.send('Chirp! ^~^');
     }
-  else if (message.content === 'Hey Smol Birb! Ready to show the world what you can do now?') {
-    message.channel.send('Uh-huh! You betcha, Corvus!');
-    }
-  else if (message.content === "Great. Let's start.") {
-    message.channel.send("Ohboyohboyohboy! I can't wait!");
-    }
-  else if (message.content === 'Smol Birb, what are you?') {
-    message.channel.send('Oh haiiiii \message.author\!!!'),
-    message.channel.send("I'm just a smol birb, silly :hatching_chick: Don't mind me ^~^ Cheep!");
-    }
-  else if (message.content === 'Smol Birb, how flexible are you?') {
-    message.channel.send("Well I can't touch my toes, if that's what you mean  >.<  Oh, and I can't respond to anything that isn't worded juuuust right, so please be specific! Sorry about that!");
-    }
-  else if (message.content === 'Who am I?') {
-    message.channel.send('You silly goose!'),
-    message.channel.send('You forgot your own name?'),
-    message.channel.send('Your name is ${member}, silly xD');
-  }
-  else if (message.content === 'Smol Birb, join my channel') {
-    voiceChannel.join()
-      .then(connection => {
-        const dispatcher = connection.playFile('339326__inspectorj__bird-whistling-a.wav');
-      })
-            })
-  });
+});
   
 client.login(process.env.BOT_TOKEN);
