b156fa3b9743f513ea5b71e823ea2ec23f2fd08a
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-17 23:04:36 -0400

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -18,7 +18,7 @@ bot.on('message', message => {
   if (message.author.equals(bot.user)) return;
   
   if (message.content === "^~^") {
-      message.channel.sendMessage("^~^");
+      message.channel.send("^~^");
       return;
   }
   
@@ -28,16 +28,16 @@ bot.on('message', message => {
   
   switch (args[0].toLowerCase()) {
     case "cheep!":
-        message.channel.sendMessage("Chirp! ^~^");
+        message.channel.send("Chirp! ^~^");
         break;
     case "info":
-        message.channel.sendMessage("Oh haiiiii!!! I'm Smol Birb, your local chirper and cheeper! I can't do much right now, but Corvus and I are hard at work trying to fix that! If you have any suggestions, please keep them to yourself! xD Lol jk we would love to hear your input. Feel free to DM @DarthCorvus#7351 with any suggestions for making me better. Thanks! ^~^");
+        message.channel.send("Oh haiiiii!!! I'm Smol Birb, your local chirper and cheeper! I can't do much right now, but Corvus and I are hard at work trying to fix that! If you have any suggestions, please keep them to yourself! xD Lol jk we would love to hear your input. Feel free to DM @DarthCorvus#7351 with any suggestions for making me better. Thanks! ^~^");
         break;
-    case "penny for your thoughts?":
-        message.channel.sendMessage(penny[Math.floor(Math.random() * penny.length)]);
+    case "cheep of the day":
+        message.channel.send(penny[Math.floor(Math.random() * penny.length)]);
         break;
     default:
-        message.channel.sendMessage("Ummm... >.< I'm sorry! I don't know that one! Do you, uh... Want to try a different one?");
+        message.channel.send("Ummm... >.< I'm sorry! I don't know that one! Do you, uh... Want to try a different one?");
         break;
   }
   
