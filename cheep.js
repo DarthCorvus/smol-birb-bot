ff045ce3b48c18a9ad1f187249a869853d767847
AJ Manning <37051778+DarthCorvus@users.noreply.github.com>
2018-03-17 22:19:56 -0400

    Update cheep.js

--- cheep.js
+++ cheep.js
@@ -21,6 +21,8 @@ bot.on('message', message => {
     case "info":
         message.channel.sendMessage("Oh haiiiii!!! I'm Smol Birb, your local chirper and cheeper! I can't do much right now, but Corvus and I are hard at work trying to fix that! If you have any suggestions, please keep them to yourself! xD Lol jk we would love to hear your input. Feel free to DM @DarthCorvus#7351 with any suggestions for making me better. Thanks! ^~^");
         break;
+    default:
+        message.channel.sendMessage("Ummm... >.< I'm sorry! I don't know that one! Do you, uh... Want to try a different one?");
   }
   
 });
